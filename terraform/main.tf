540827677722e19d5fb66986501a7b8541b5691f
roni <roniadmon@gmail.com>
2024-01-06 16:20:37 +0200

    running db verification on cd, adding ip and dns record to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1"
   profile = "default"
 }
 
-resource "aws_security_group" "recommendations" {
-  name        = "recommendations-sg"
-  
+resource "aws_security_group" "recommendations_sg" {
+  name = "recommendations-sg"
+
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -25,18 +14,17 @@ resource "aws_security_group" "recommendations" {
   }
 }
 
-resource "aws_key_pair" "recommendations" {
-  key_name   = "recommendations-key"
+resource "aws_key_pair" "recommendations_kp" {
+  key_name   = "recommendations-keypair"
   public_key = "ssh-rsa recommendations_ssh_key"
 }
 
 # Create an EC2 instance
-resource "aws_instance" "recommendations" {
-  name          = "recommendations"
-  ami           = "ami-02cad064a29d4550c " 
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.recommendations.key_name
-  security_groups = [aws_security_group.recommendations.name]
+resource "aws_instance" "recommendations_ec2" {
+  ami             = "ami-02cad064a29d4550c "
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.recommendations_kp.key_name
+  security_groups = [aws_security_group.recommendations_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -47,6 +35,23 @@ resource "aws_instance" "recommendations" {
               EOF
 
   tags = {
-    Name = "recommendations-instance"
+    Name = "recommendations"
   }
+}
+
+resource "aws_eip" "recommendations_eip" {
+  vpc      = true
+  instance = aws_instance.recommendations_ec2.id
+}
+
+resource "aws_route53_zone" "recommendations_zone" {
+  name = "recommendations.com"
+}
+
+resource "aws_route53_record" "www-restaurants" {
+  zone_id = aws_route53_zone.recommendations_zone.zone_id
+  name    = "www.restaurants.recommendations.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.recommendations_eip.public_ip]
 }
